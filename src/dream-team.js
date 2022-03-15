2c5f302114df99658b2e6414249d35daa149bd0a
Roman Klepov <rklepov@gmail.com>
2022-03-15 23:59:40 +0300

    feat: resolve dream-team task
    https://github.com/rklepov/basic-js#dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,33 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    if (!(members instanceof Array)) return false;
+    if (members.length <= 0) return false;
+    return members
+        .map((x) => {
+            if ("string" === typeof x) {
+                return x.trim()[0].toUpperCase();
+            } else {
+                return "";
+            }
+        })
+        .sort()
+        .join("");
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
