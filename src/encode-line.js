1b4f3482fff895f898e2468baf9d573dc55c89e8
Roman Klepov <rklepov@gmail.com>
2022-03-16 19:37:01 +0300

    feat: resolve encode-line task
    https://github.com/rklepov/basic-js#st-encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    const result = str
+        .match(/(.)\1*/g)
+        ?.map((x) => (x.length > 1 ? x.length + x[0] : x[0]))
+        .join("");
+
+    return result || "";
 }
 
 module.exports = {
-  encodeLine
+    encodeLine,
 };
