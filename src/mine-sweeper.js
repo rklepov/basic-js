73320b9bbb681f32b74cb977a1df63852dec7526
Roman Klepov <rklepov@gmail.com>
2022-03-29 22:42:59 +0300

    feat: resolve mine-sweeper task
    https://github.com/rklepov/basic-js#st-minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+    const field = Array(matrix.length)
+        .fill(null)
+        .map((_, i) => Array(matrix[i].length).fill(0));
+
+    for (i = 0; i < matrix.length; ++i) {
+        for (j = 0; j < matrix[i].length; ++j) {
+            if (matrix[i][j]) {
+                field[i][j] = +matrix[i][j];
+                continue;
+            }
+
+            for (let di of [-1, 0, 1]) {
+                for (let dj of [-1, 0, 1]) {
+                    field[i][j] += matrix[i + di]?.[j + dj] ?? 0;
+                }
+            }
+        }
+    }
+
+    return field;
 }
 
 module.exports = {
-  minesweeper
+    minesweeper,
 };
