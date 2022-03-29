9c7842353693330df99737fecbcaccae04e92221
Roman Klepov <rklepov@gmail.com>
2022-03-30 00:35:59 +0300

    feat: resolve simple-chain task
    https://github.com/rklepov/basic-js#chain-maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,62 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        if (value === null) {
+            value = String(value);
+        }
+        this.chain.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (!Number.isInteger(position) || position < 1 || position > this.chain.length) {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let strChain = this.chain.join(" )~~( ");
+        if (strChain) {
+            strChain = ["( ", strChain, " )"].join("");
+        }
+        this.chain = [];
+        return strChain;
+    },
 };
 
 module.exports = {
-  chainMaker
+    chainMaker,
 };
+
+console.log(
+    chainMaker
+        .reverseChain()
+        .reverseChain()
+        .reverseChain()
+        .addLink(NaN)
+        .reverseChain()
+        .addLink(null)
+        .addLink(1.233)
+        .addLink(true)
+        .addLink(false)
+        .removeLink(3)
+        .addLink(1.233)
+        .finishChain(),
+    "\n",
+    "( NaN )~~( null )~~( true )~~( false )~~( 1.233 )"
+);
