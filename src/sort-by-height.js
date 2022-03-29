5286b9d42a4969d3b660c0270b348c6a6193547f
Roman Klepov <rklepov@gmail.com>
2022-03-29 23:46:24 +0300

    feat: resolve sort-by-height task
    https://github.com/rklepov/basic-js#st-sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,13 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
+
+function getAllIndexes(arr, val) {
+    const indexes = [];
+    i = -1;
+    while ((i = arr.indexOf(val, i + 1)) != -1) {
+        indexes.push(i);
+    }
+    return indexes;
+}
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +20,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+    const GAP_LABEL = -1;
+    const gapIx = getAllIndexes(arr, GAP_LABEL);
+    const sorted = arr.filter((x) => x !== GAP_LABEL).sort((a, b) => a - b);
+    return arr.map((x, i) => (gapIx.includes(i) ? GAP_LABEL : sorted[i - gapIx.reduce((a, j) => a + (j < i), 0)]));
 }
 
 module.exports = {
-  sortByHeight
+    sortByHeight,
 };
