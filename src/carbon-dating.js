fd872a8c2ec2a9646c8839cbb5b4587c82cde353
Roman Klepov <rklepov@gmail.com>
2022-04-01 10:23:05 +0300

    feat: resolve carbon-dating task
    https://github.com/rklepov/basic-js#carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// carbon-dating.js
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,32 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    if (typeof sampleActivity !== "string") return false;
+
+    const activity = Number(sampleActivity);
+    if (!activity) return false;
+    if (activity < 0 || activity > MODERN_ACTIVITY) return false;
+
+    const rate = Math.log(2) / HALF_LIFE_PERIOD;
+    const years = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / rate);
+
+    return years;
 }
 
 module.exports = {
-  dateSample
+    dateSample,
 };
+
+//__EOF__
